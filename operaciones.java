820643fa761a5e2dd27b1281703c9ab9d067264d
Victor Luna <vicluna003@gmail.com>
2016-03-29 13:43:19 -0600

    Avances calculadora JAVA

---
+++ operaciones.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author victor
+ */
+public class operaciones {
+    
+    
+    double x=0,y=0,res=0;
+
+    public operaciones() {
+    
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getRes() {
+        return res;
+    }
+
+    public void setRes(double res) {
+        this.res = res;
+    }
+    
+    
+    
+}
